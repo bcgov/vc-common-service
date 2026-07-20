81d018140ff3e6772b814e7bec74008da7596b29
jamshale <jamiehalebc@gmail.com>
2026-07-20 14:33:53 -0700

    fix: linting
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- apps/vc-common-service/src/tenant-user/dto/update-tenant-user.dto.ts
+++ apps/vc-common-service/src/tenant-user/dto/update-tenant-user.dto.ts
@@ -2,6 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 
 import { CreateTenantUserDto } from './create-tenant-user.dto';
 
-export class UpdateTenantUserDto extends PartialType(
-  CreateTenantUserDto,
-) {}
+export class UpdateTenantUserDto extends PartialType(CreateTenantUserDto) {}
